233c0cb85025ce376c57092e59e4afa5cba72917
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-15 20:11:18 -0600

    added PAUSE, comments & increased duration

--- lol.py
+++ lol.py
@@ -8,8 +8,14 @@ with other applications.
 >>> https://github.com/asweigart/pyautogui 
 '''
 
-pixel = 20
+# Sets a 2 second pause after each PyAutoGUI call:
+pyautogui.PAUSE = 2
+
+# How manu pixels PyAutoGUI will use to move in screen
+pixel = 200
 
 while True:
-    pixel *= -1
-    pyautogui.moveRel(0, pixel, duration=1)
+    pixel *= -1 # move 200 pixels in opposite directions 
+    pyautogui.moveRel(0, pixel, duration=5) # duration = speed of mouse movement
+
+# End program by control + c via CLI
